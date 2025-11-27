49b4db312c1ac6e860e2dcadca24c201c8b0d02e
Jbamidi <jassubamidi24@gmail.com>
2025-11-26 22:26:34 -0800

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -17,7 +17,7 @@ import {
 function Header() {
   const {isDark} = useContext(StyleContext);
   const viewExperience = workExperiences.display;
-  const viewOpenSource = openSource.display;
+  const viewOpenSource = openSource.display; // we reuse this flag to show Projects
   const viewSkills = skillsSection.display;
   const viewAchievement = achievementSection.display;
   const viewBlog = blogSection.display;
@@ -32,55 +32,60 @@ function Header() {
           <span className="logo-name">{greeting.username}</span>
           <span className="grey-color">/&gt;</span>
         </a>
+
         <input className="menu-btn" type="checkbox" id="menu-btn" />
-        <label
-          className="menu-icon"
-          htmlFor="menu-btn"
-          style={{color: "white"}}
-        >
+        <label className="menu-icon" htmlFor="menu-btn" style={{color: "white"}}>
           <span className={isDark ? "navicon navicon-dark" : "navicon"}></span>
         </label>
+
         <ul className={isDark ? "dark-menu menu" : "menu"}>
           {viewSkills && (
             <li>
               <a href="#skills">Skills</a>
             </li>
           )}
+
           {viewExperience && (
             <li>
-              <a href="#experience">Work Experiences</a>
+              <a href="#experience">Experience</a>
             </li>
           )}
+
           {viewOpenSource && (
             <li>
-              <a href="#opensource">Open Source</a>
+              <a href="#projects">Projects</a>
             </li>
           )}
+
           {viewAchievement && (
             <li>
               <a href="#achievements">Achievements</a>
             </li>
           )}
+
           {viewBlog && (
             <li>
               <a href="#blogs">Blogs</a>
             </li>
           )}
+
           {viewTalks && (
             <li>
               <a href="#talks">Talks</a>
             </li>
           )}
+
           {viewResume && (
             <li>
               <a href="#resume">Resume</a>
             </li>
           )}
+
           <li>
-            <a href="#contact">Contact Me</a>
+            <a href="#contact">Contact</a>
           </li>
+
           <li>
-            {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
             <a>
               <ToggleSwitch />
             </a>
@@ -90,4 +95,5 @@ function Header() {
     </Headroom>
   );
 }
+
 export default Header;
